0a08a4d11b362862dc6c3ad39e78b9f679fae1e9
Pavel Gorbach <polgorbach@gmail.com>
2022-12-26 10:09:30 +0300

    feat: move data fetching to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { Product } from 'dummyjson-api/models'
+import { getProducts } from 'dummyjson-api'
 
 import PageLayout from 'components/PageLayout'
 import ProductsList from 'pages/ProductsList'
@@ -7,11 +11,23 @@ import Cart from 'pages/Cart'
 import NoMatch from 'pages/NoMatch'
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  // TODO: move to the application root
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const data = await getProducts()
+      setProducts(data.products)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<PageLayout />}>
-          <Route index element={<ProductsList />} />
+          <Route index element={<ProductsList products={products} />} />
           <Route path="product-details/:id" element={<ProductDetails />} />
           <Route path="cart" element={<Cart />} />
           <Route path="*" element={<NoMatch />} />
--- src/pages/ProductsList/index.tsx
+++ src/pages/ProductsList/index.tsx
@@ -1,26 +1,15 @@
-import { useEffect, useState } from 'react'
-
-import { Product } from 'dummyjson-api/models'
 import PageTitle from 'components/PageTitle'
-import { getProducts } from 'dummyjson-api'
+import { Product } from 'dummyjson-api/models'
 
-export default function ProductsList() {
-  const [products, setProducts] = useState<Product[]>()
-
-  // TODO: move to the application root
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const data = await getProducts()
-      setProducts(data.products)
-    }
-
-    fetchProducts()
-  }, [])
-
-  return (
-    <div>
-      <PageTitle text="Products List" />
-      {products && products.map((product) => <div key={product.id}>{product.title}</div>)}
-    </div>
-  )
+type Props = {
+  products: Product[]
 }
+
+const ProductsList = (p: Props) => (
+  <div>
+    <PageTitle text="Products List" />
+    {p.products && p.products.map((product) => <div key={product.id}>{product.title}</div>)}
+  </div>
+)
+
+export default ProductsList
