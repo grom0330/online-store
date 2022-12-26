bc68b6208b4345dcbd2f75903213698e055fcbc6
Pavel Gorbach <polgorbach@gmail.com>
2022-12-26 09:30:07 +0300

    refactor: remove App.css; adjust index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
 
 import PageLayout from 'components/PageLayout'
 import ProductsList from 'pages/ProductsList'
