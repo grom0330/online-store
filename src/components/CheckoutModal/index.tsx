22a1851634fa45981859cc69d68ce7272c423a23
grom0330 <tokarev-oleg2004@mail.ru>
2023-01-06 14:34:02 +0300

    feat: complete CheckoutModal

--- src/components/CheckoutModal/index.tsx
+++ src/components/CheckoutModal/index.tsx
@@ -1,9 +1,48 @@
+import { FormEvent, useEffect, useState } from 'react'
+import trueImg from '../../assets/images/true.png'
+import cardImg from '../../assets/images/credit-card.png'
+import visa from '../../assets/images/visa.png'
+import masterCard from '../../assets/images/MasterCard.png'
+import maestro from '../../assets/images/maestro.png'
+import americanExpress from '../../assets/images/american-express.png'
+import PageTitle from 'components/PageTitle'
+import { useNavigate } from 'react-router'
+
+const imagesPath = [cardImg, visa, masterCard, maestro, americanExpress]
+
 export type CheckoutModelProps = {
   p: boolean
   total: number
 }
 
 export default function CheckoutModel({ p, total }: CheckoutModelProps) {
+  const navigate = useNavigate()
+  const [form, setForm] = useState(p)
+  const [validity, setValidity] = useState('')
+  const [cardNumber, setCardNumber] = useState(0)
+
+  const validityHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const per = validity.concat('/', e.target.value.slice(-1))
+    if (validity.length == 2 && !(e.target.value.length === 1)) {
+      setValidity(per)
+      return
+    } else setValidity(e.target.value)
+  }
+
+  const cardNumberHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const per = Number(e.target.value.slice(0, 1))
+    if (per === 5) setCardNumber(2)
+    else if (per === 4) setCardNumber(1)
+    else if (per === 6) setCardNumber(3)
+    else if (per === 3) setCardNumber(4)
+    else setCardNumber(0)
+  }
+
+  const buttonHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setForm(false)
+  }
+
   return (
     <div
       className={
@@ -12,12 +51,20 @@ export default function CheckoutModel({ p, total }: CheckoutModelProps) {
           : 'hidden'
       }
     >
-      <div className="flex items-center w-2/5">
-        <form className="w-full rounded-3xl p-10 bg-white shadow-xl">
+      <div className="absolute top-1/4 items-center justify-center w-64 h-52 bg-white flex flex-col gap-6">
+        <img className="w-20 h-20" src={trueImg} />
+        <PageTitle text={'Order is processed'}></PageTitle>
+      </div>
+      <div className={form ? 'flex items-center w-2/5 z-50' : 'hidden'}>
+        <form
+          onSubmit={(e) => buttonHandler(e)}
+          className="w-full rounded-3xl p-10 bg-white shadow-xl"
+        >
           <p className="text-gray-800 font-medium text-xl">Customer information</p>
           <div className="">
             <input
               required
+              pattern="^[a-zA-Z]{3,}( [a-zA-Z]{3,})+$"
               className="w-full mt-4 px-2 py-1 text-gray-700 bg-gray-200 rounded"
               id="name"
               name="name"
@@ -27,6 +74,8 @@ export default function CheckoutModel({ p, total }: CheckoutModelProps) {
           </div>
           <div className="mt-2">
             <input
+              required
+              pattern="^\+[0-9]{9,}"
               className="w-full px-2 py-1 text-gray-700 bg-gray-200 rounded"
               id="phone"
               name="phone"
@@ -36,6 +85,8 @@ export default function CheckoutModel({ p, total }: CheckoutModelProps) {
           </div>
           <div className="mt-2">
             <input
+              required
+              pattern="^[a-zA-Z]{5,} [a-zA-Z]{5,}( [a-zA-Z]{5,})+$"
               className="w-full px-2 py-1 text-gray-700 bg-gray-200 rounded"
               id="address"
               name="address"
@@ -45,6 +96,8 @@ export default function CheckoutModel({ p, total }: CheckoutModelProps) {
           </div>
           <div className="mt-2">
             <input
+              required
+              pattern="^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$"
               className="w-full px-2 py-1 text-gray-700 bg-gray-200 rounded"
               id="email"
               name="email"
@@ -53,32 +106,44 @@ export default function CheckoutModel({ p, total }: CheckoutModelProps) {
             />
           </div>
           <p className="mt-4 text-gray-800 text-xl font-medium">Payment information</p>
-          <div className="">
-            <input
-              className="w-full mt-4 px-2 py-1 text-gray-700 bg-gray-200 rounded"
-              id="cus_name"
-              name="cus_name"
-              type="text"
-              placeholder="Card Number"
-              aria-label="Name"
-            />
+          <div className=" mt-4 flex items-center w-full">
+            <img src={imagesPath[cardNumber]} className="h-10 w-10" />
+            <div className="ml-3 grow">
+              <input
+                required
+                className="w-full px-2 py-1 text-gray-700 bg-gray-200 rounded"
+                pattern="[0-9]{16}"
+                onChange={(e) => cardNumberHandler(e)}
+                id="cardNumber"
+                name="cardNumber"
+                type="text"
+                placeholder="Card Number"
+              />
+            </div>
           </div>
           <div className="flex justify-between">
             <div className="">
               <input
+                required
+                pattern="^(?:[0-1][1-2])\/(?:[0-2]\d|3[0-1])$"
+                value={validity}
+                onChange={(e) => validityHandler(e)}
+                maxLength={5}
                 className="w-2/3 mt-4 px-2 py-1 text-gray-700 bg-gray-200 rounded"
-                id="cus_name"
-                name="cus_name"
+                id="validity"
+                name="validity"
                 type="text"
                 placeholder="Validity"
-                aria-label="Name"
               />
             </div>
             <div className="flex justify-end">
               <input
+                required
+                pattern="^[0-9]{3}$"
+                maxLength={3}
                 className="w-1/3 mt-4 px-2 py-1 text-gray-700 bg-gray-200 rounded"
-                id="cus_name"
-                name="cus_name"
+                id="cvv"
+                name="cvv"
                 type="text"
                 placeholder="CVV"
                 aria-label="Name"
@@ -87,8 +152,8 @@ export default function CheckoutModel({ p, total }: CheckoutModelProps) {
           </div>
           <div className="mt-4">
             <button
-              className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
               type="submit"
+              className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
             >
               ${total}
             </button>
